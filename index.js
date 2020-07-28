b88a55f0577b64e0491f7ded8e6173501aae40fe
genii0826 <45891745+genii0826@users.noreply.github.com>
2020-07-28 14:11:11 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.env.token
+const token = process.env.token;
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('켰다.');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+client.on('message', (message) => {
+  if(message.content === 'ping') {
+    message.reply('pong');
   }
 });
 
-client.login('NzM3NDg0MzcxNTcyOTQ5MDEy.Xx-B1w.bW25CCemmdRdtkOrqY6UEfuNLKk');
+client.login(token);
